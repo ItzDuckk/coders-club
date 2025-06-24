621d36a7d2c844fe26e6391f65d09d391cdafbc8
ItzDuckk <guy.shamir1788@gmail.com>
2025-06-24 19:58:10 +0300

    edited binery_convertor

--- Work/binery_thing/binery_convertor.py
+++ Work/binery_thing/binery_convertor.py
@@ -1,24 +1,26 @@
-def twos_complement(val, size):
-    # range-check
-    if val < 0:
-        print("Error: pass a non-negative value (the function negates it).")
-        return
-    if val >= (2 ** size):
-        print(f"Error: {val} needs more than {size} bits.")
-        return
+def get_input():
+    positive_decimal = int(input("Enter a positive integer:"))
+    byte_size = int(input("Enter the memory size in bits:"))
+    try:   
+        input > 0
+    except:
+        print("Error: input must be Positive")
+    
+    return positive_decimal,byte_size
 
-    mask = (2 ** size) - 1
-    flipped = (~val + 1) & mask
+def decimal_to_binary(decimal,bits):
+    return str(bin(decimal))[2::1].zfill(bits)
 
-    output = ''
+def twos_complement(decimal,bits):
+   inverted_binary = ''.join('1' if bit == '0' else '0' for bit in decimal)
+   decimal_inverted = int(inverted_binary,base=2) + 1
+   return decimal_to_binary(decimal_inverted,bits)
 
-    for i in  range(size-1 , -1 , -1):
-        if (flipped >> i) & 1:
-            output += '1'
-        else:
-            output += '0'
+def main():
+    decimal,memory = get_input()
+    binary = decimal_to_binary(decimal,memory)
+    inverted = twos_complement(binary,memory)
+    print(inverted)
 
-    print(output)
-
-twos_complement(42, 8)
-
+if __name__ == "__main__":
+    main()
